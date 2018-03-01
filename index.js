abbdd8e639b011c6a14cbe7a1cae27ac1ab14172
q20010012001001 <1223118063@qq.com>
2018-03-01 13:27:31 +0800

    first

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+  var imgload = function(img) {
+    var ref = $.Deferred();
+    if (img.complete) {
+      ref.resolve();
+    } else {
+      img.onload = function(event) {
+        ref.resolve(event)
+      }
+
+      img.onerror = function(err) {
+        ref.reject(err)
+      }
+    }
+    return ref;
+  }
+
+  var imageCenter = function(domList, mode) {
+    $(domList).each(function(index,item) {
+      var img = item.children[0];
+      var itemW = item.offsetWidth;
+      var itemH = item.offsetHeight;
+      var itemR = itemW / itemH;
+      imgload(img).then(function() {
+        var imgW = img.naturalWidth;
+        var imgH = img.naturalHeight;
+        var imgR = imgW / imgH;
+        var resultMode = null;
+
+        switch (mode) {
+
+          case 'wspectFill':
+            resultMode = itemR > imgR ? 'aspectFill-x' : 'aspectFill-y'
+            break;
+          default:
+
+        }
+
+        $(img).addClass(resultMode)
+
+      })
+    })
+
+
+  }
