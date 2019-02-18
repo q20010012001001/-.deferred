3f09188e8c3edf7f1f45435af9c239a1d92ea79d
q20010012001001 <1223118063@qq.com>
2019-02-18 11:20:25 +0800

    Signed-off-by: q20010012001001 <1223118063@qq.com>

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # -.deferred
 $.deferred做图片大小宽度限制
-
-jq $.deferred引用
-
-js原声 forEach 在IE报错....
-换成jq中 each 循环 ....
+兼容IE8, 依赖jq
--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 
 
-  var imgload = function(img) {
-    var ref = $.Deferred();
-    if (img.complete) {
-      ref.resolve();
-    } else {
-      img.onload = function(event) {
-        ref.resolve(event)
-      }
-
-      img.onerror = function(err) {
-        ref.reject(err)
-      }
+var imgload = function (img) {
+  var ref = $.Deferred();
+  if (img.complete) {
+    ref.resolve();
+  } else {
+    img.onload = function (event) {
+      ref.resolve(event)
     }
-    return ref;
+
+    img.onerror = function (err) {
+      ref.reject(err)
+    }
   }
+  return ref;
+}
 
-  var imageCenter = function(domList, mode) {
-    $(domList).each(function(index,item) {
-      var img = item.children[0];
-      var itemW = item.offsetWidth;
-      var itemH = item.offsetHeight;
-      var itemR = itemW / itemH;
-      imgload(img).then(function() {
-        var imgW = img.naturalWidth;
-        var imgH = img.naturalHeight;
-        var imgR = imgW / imgH;
-        var resultMode = null;
-
-        switch (mode) {
-
-          case 'wspectFill':
-            resultMode = itemR > imgR ? 'aspectFill-x' : 'aspectFill-y'
-            break;
-          default:
-
-        }
-
-        $(img).addClass(resultMode)
-
-      })
+// mode选项一般选为wspectFill
+var imageCenter = function (domList, mode) {
+  
+  $(domList).each(function (index,item) {
+    var img = item.children[0];
+    var itemW = item.offsetWidth;
+    var itemH = item.offsetHeight;
+    var itemR = itemW / itemH;
+    imgload(img).then(function () {
+      var imgW = img.naturalWidth;
+      var imgH = img.naturalHeight;
+      var imgR = imgW / imgH;
+      var resultMode = null;
+      switch (mode) {
+        case 'aspectFill':
+          resultMode = imgR > 1 ? 'aspectFill-x' : 'aspectFill-y';
+          break;
+        case 'wspectFill':
+          resultMode = itemR > imgR ? 'aspectFill-x' : 'aspectFill-y'
+          break;
+        default:
+      }
+      $(img).addClass(resultMode);
     })
+  })
+}
 
-
-  }
